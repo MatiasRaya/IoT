1a27bde25bf502586a27fc2efa6cd64e4f1f37a3
MatiasRaya <matias.raya@mi.unc.edu.ar>
2022-06-21 06:21:28 -0300

    Ejemplo de uso de jsonify

---
+++ Ejemplos/FLASK/Ejemlo-3/app.py
@@ -0,0 +1,41 @@
+from crypt import methods
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/person", methods=['POST', 'GET'])
+def handle_person():
+    if request.method == 'POST':
+        return "Se recibió un POST"
+    else:
+        return "Se recibió un GET"
+
+@app.route("/persons")
+def persons():
+    person1 = {
+        "name": "Bob"
+    }
+    return jsonify(person1)
+
+@app.route("/error")
+def error():
+    contenido = {
+        "detalles": "Hubo un error en la solicitud"
+    }
+    resp = jsonify(contenido)
+    resp.status_code = 400
+    return resp
+
+@app.route("/errors")
+def errors():
+    contenido = {
+        "detalles": "Hubo un error en la solicitud"
+    }
+    return jsonify(contenido), 400
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',debug=True)
