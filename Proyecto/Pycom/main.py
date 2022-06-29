e9db25bee4765c9615fe4eeb6d04963f19bbf4f7
MatiasRaya <matias.raya@mi.unc.edu.ar>
2022-06-29 09:35:54 -0300

    Se agrego el envio de datos

--- Proyecto/Pycom/main.py
+++ Proyecto/Pycom/main.py
@@ -14,7 +14,6 @@ from urequests import Response
 
 
 SERVER_ADDRESS = "http://192.168.1.142" # LCD
-# SERVER_ADDRESS = "http://192.168.0.13" # Casa
 # SERVER_ADDRESS = "" # APP HEROKU
 SERVER_PORT = "5000"
 
@@ -31,9 +30,12 @@ NO_COLOUR = 0x000000
 
 pycom.heartbeat(False)
 
+pycom.rgbled(ORANGE)
+time.sleep(2)
+pycom.rgbled(NO_COLOUR)
+
 # WiFi connectation
 wlan = WLAN(mode=WLAN.STA)
-# wlan.connect('RAYA 2.4', auth=(WLAN.WPA2, 'Rayaplasencia1996'))
 wlan.connect('LCD3', auth=(WLAN.WPA2, '1cdunc0rd0ba'))
 print('Network found!')
 while not wlan.isconnected():
@@ -48,6 +50,7 @@ py = Pycoproc(Pycoproc.PYSENSE)
 pySensor = Sensors(py)
 
 data_sensor = {
+    'nodo' : 1,
     'light' : pySensor.get_light(),
     'humidity' : pySensor.get_humidity(),
     'temperature' : pySensor.get_temperature(),
@@ -62,11 +65,12 @@ rate = {
     'altitude_rate': 1
 }
 
+# Chrono definition and inicialization
 chrono = Timer.Chrono()
 
-def transmission_handler(alarm):
-    alarm.cancel()
-    alarm = Timer.Alarm(transmission_handler, rate['transmission_rate'], periodic=True)
+# def transmission_handler(alarm):
+#     alarm.cancel()
+#     alarm = Timer.Alarm(transmission_handler, rate['transmission_rate'], periodic=True)
 
 def light_handler(alarm):
     alarm.cancel()
@@ -90,7 +94,7 @@ def altitude_handler(alarm):
 
 chrono.start()
 
-transmission_alarm = Timer.Alarm(transmission_handler, rate['transmission_rate'], periodic=True)
+# transmission_alarm = Timer.Alarm(transmission_handler, rate['transmission_rate'], periodic=True)
 light_rate = Timer.Alarm(light_handler, rate['light_rate'], periodic=True)
 humidity_rate = Timer.Alarm(humidity_handler, rate['humidity_rate'], periodic=True)
 temperature_rate = Timer.Alarm(temperature_handler, rate['temperature_rate'], periodic=True)
@@ -98,53 +102,37 @@ altitude_rate = Timer.Alarm(altitude_handler, rate['altitude_rate'], periodic=Tr
 
 alarm_sets = []
 
-alarm_sets.append([transmission_alarm, transmission_handler, 'transmission_rate'])
+# alarm_sets.append([transmission_alarm, transmission_handler, 'transmission_rate'])
 alarm_sets.append([light_rate, light_handler, 'light_rate'])
 alarm_sets.append([humidity_rate, humidity_handler, 'humidity_rate'])
 alarm_sets.append([temperature_rate, temperature_handler, 'temperature_rate'])
 alarm_sets.append([altitude_rate, altitude_handler, 'altitude_rate'])
 
-def stored_data(samples, interval):
+def stored_data(interval):
     store_data = {}
     time.sleep(interval)
-    aux = dict()
-    aux["ts"] = int(time.time())
-    aux["values"] = data_sensor
-    store_data = aux
-    print(aux)
-    print('\n------------------------------------------------------\n')
-    print(store_data)
-    print('\n------------------------------------------------------\n')
+    store_data = data_sensor
     json_store_data = ujson.dumps(store_data)
     return json_store_data
 
+def post_method(address, raw_data):
+    response = urequests.post(address, data=raw_data)
+    return response
+count = 0
 while True:
-    stored_data(2,5)
+    store_data = stored_data(5)
+    try:
+        response = post_method(SERVER_ADDRESS + ":" + SERVER_PORT + "/data", store_data)
+        pycom.rgbled(GREEN)
+        time.sleep(1)
+        pycom.rgbled(NO_COLOUR)
+        count += 1
+    except Exception as e:
+        print(e)
+        response = ''
+        print("POST attempet failed.")
+        pycom.rgbled(RED)
+        time.sleep(1)
+        pycom.rgbled(NO_COLOUR)
 
-# def post_method(address, raw_data):
-#     response = urequests.post(address, data=raw_data)
-#     return response
-
-
-# count = 0
-
-# while True:
-#     # time.sleep(1)
-#     try:
-#         response = post_method(SERVER_ADDRESS + ":" + SERVER_PORT + "/tasks", stored_data())
-#         # print(response.content)
-#         # response.close()
-#         pycom.rgbled(0x007f00)
-#         count += 1
-#         # time.sleep(2)
-#         pycom.rgbled(0x7f007f)
-#         # time.sleep(3)
-#     except Exception as e:
-#         print(e)
-#         response = ''
-#         print("POST attempet failed.")
-#         pycom.rgbled(0x00007f)
-#         time.sleep(2)
-#     # time.sleep(1)
-#     pycom.rgbled(0x4a007f)
-#     print(count)
+    print(count)
