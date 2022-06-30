0eae77b55e022a2c7a15baaa3cc9371561dedfbb
MatiasRaya <matias.raya@mi.unc.edu.ar>
2022-06-30 08:19:17 -0300

    Se agrego seccion de consulta y de borrado

--- Proyecto/Server/api_server.py
+++ Proyecto/Server/api_server.py
@@ -1,5 +1,6 @@
 from crypt import methods
 import json
+from sqlalchemy import or_
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
@@ -14,12 +15,18 @@ ma = Marshmallow(app)
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nodo = db.Column(db.Integer)
+    iteration = db.Column(db.Integer)
+    lightB = db.Column(db.Integer)
+    lightR = db.Column(db.Integer)
     humidity = db.Column(db.Integer)
     temperature = db.Column(db.Integer)
     altitude = db.Column(db.Integer)
 
-    def __init__(self, nodo, humidity, temperature, altitude):
+    def __init__(self, nodo, iteration, lightB, lightR, humidity, temperature, altitude):
         self.nodo = nodo
+        self.iteration = iteration
+        self.lightB = lightB
+        self.lightR = lightR
         self.humidity = humidity
         self.temperature = temperature
         self.altitude = altitude
@@ -28,7 +35,7 @@ db.create_all()
 
 class TaskSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'nodo', 'humidity', 'temperature', 'altitude')
+        fields = ('id', 'nodo', 'iteration', 'lightB', 'lightR', 'humidity', 'temperature', 'altitude')
 
 task_schema = TaskSchema()
 tasks_schema = TaskSchema(many=True)
@@ -37,24 +44,36 @@ tasks_schema = TaskSchema(many=True)
 def create_data():
     information = request.get_json(force=True)
     nodo = information['nodo']
+    iteration = information['iteration']
+    lightB = information['lightB']
+    lightR = information['lightR']
     humidity = information['humidity']
     temperature = information['temperature']
     altitude = information['altitude']
 
-    new_task = Task(nodo, humidity, temperature, altitude)
+    new_task = Task(nodo, iteration, lightB, lightR, humidity, temperature, altitude)
 
     db.session.add(new_task)
     db.session.commit()
 
-    return jsonify({'value' : 'key'})
+    return jsonify({'key' : 'value'})
 
 @app.route('/consultation/<id>', methods=['GET'])
 def create_consult(id):
     all_tasks = Task.query.filter_by(nodo=id).all()
     result = tasks_schema.dump(all_tasks)
     size = len(result)
+    all = Task.query.filter(Task.nodo==id, Task.iteration==size-1)
+    resul = tasks_schema.dump(all)
+    print(resul)
     print(size)
-    return jsonify(result),len(result)
+    return jsonify(resul)
+
+@app.route('/delete/<id>', methods=['GET'])
+def delte_table(id):
+    db.session.query(Task).filter(Task.nodo==id).delete()
+    db.session.commit()
+    return jsonify({'key' : 'value'})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000,debug=True)
