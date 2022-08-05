d597a8f5060a4660b2866ddeed58118df2b87a6a
MatiasRaya <matias.raya@mi.unc.edu.ar>
2022-08-05 16:55:10 -0300

    Se agrego ejemplo utilizando una red LTE

---
+++ Ejemplos/LTE/main.py
@@ -0,0 +1,43 @@
+from network import LTE
+import time
+import socket
+
+lte = LTE()
+
+# Seteamos la banda en la que vamos a operar (la cual depende de la version de nuestra FiPy) y la apn del operador de nuestra SIM
+# APN para android
+# APN Personal: datos.personal.com
+# APN Claro: igprs.claro.com.ar
+# APN Movistar: wap.gprs.unifon.com.ar //// internet.gprs.unifon.com.ar //// mms.gprs.unifon.com.ar
+# APN ios
+# APN Personal: datos.personal.com
+# APN Claro: igprs.claro.com.ar
+# APN Movistar: gprs.unifon.com.ar
+# Bandas de 4G LTE en Argentina: 2, 4, 5, 7, 8, 28, 66
+
+# Hailitamos la funcionalidad de radio y conectarse a la red LTE autorizada por la tarjeta SIM
+lte.attach(band=28, apn="datos.personal.com")
+print("attaching..",end='')
+# Consultamos si estamos conectados a la red
+while not lte.isattached():
+    time.sleep(0.25)
+    print('.',end='')
+    # Envía un comando AT directamente al módem
+    print(lte.send_at_cmd('AT!="fsm"'))         # get the System FSM
+print("attached!")
+
+# Se inicia sesion y se obtiene una direccion IP
+lte.connect()
+print("connecting [##",end='')
+# Consultamos sitenemos una sesion de datos LTE activa y si se ha obtenido ua dirección IP
+while not lte.isconnected():
+    time.sleep(0.25)
+    print('#',end='')
+    #print(lte.send_at_cmd('AT!="showphy"'))
+    print(lte.send_at_cmd('AT!="fsm"'))
+print("] connected!")
+
+# Se traduce el argumento host/port en una secuencia de 5 tuplas qe contengan los argumentos necesarios para crear un socket conectado a ese servicio
+print(socket.getaddrinfo('pybytes.pycom.io', 80))  
+# Deshabilita completamente el modem LTE, se hace para reducir el consumo de energía al minimo
+lte.deinit()
