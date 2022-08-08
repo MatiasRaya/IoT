7a575a8712ed5b3b36f9092b426483b300575e67
MatiasRaya <matias.raya@mi.unc.edu.ar>
2022-08-08 17:07:13 -0300

    Se crearon archivos para tener las conexiones a internet separadas

---
+++ Proyecto/PYSENSE/Pycom/connections.py
@@ -0,0 +1,25 @@
+import machine
+import time
+
+from network import WLAN, LTE
+
+def wifi_connection():
+    wlan = WLAN(mode=WLAN.STA)
+    wlan.connect('LCD', auth=(WLAN.WPA2, '1cdunc0rd0ba'))
+    print('Network found!')
+    while not wlan.isconnected():
+        machine.idle()
+    print('WLAN connection succeeded!')
+
+def lte_connection():
+    lte = LTE()
+    lte.attach(band=28, apn="datos.personal.com")
+    # lte.attach(band=28, apn="igprs.claro.com.ar")
+    while not lte.isattached():
+        time.sleep(0.25)
+        print('.',end='')
+    lte.connect()
+    while not lte.isconnected():
+        time.sleep(0.25)
+    print('LTE connection succeeded!')
+
